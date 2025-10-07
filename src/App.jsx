af10058cd8a384f943ede63bb344b91bb8b3f2b9
ShiroYaksh90 <aljazaeribasim@gmail.com>
2025-10-08 01:27:03 +0300

    Install & confgiure tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import "./App.css";
-
+import React from "react";
 function App() {
   return (
     <>
-      <header>
+      <header className="flex justify-between py-4 px-8">
         <div>Logo</div>
-        <ul>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   );
