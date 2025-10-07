ec7998775d3200547a3cb758fb56a134384890fe
ShiroYaksh90 <aljazaeribasim@gmail.com>
2025-10-08 01:05:46 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <div>Logo</div>
+        <ul>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+}
+
+export default App;
